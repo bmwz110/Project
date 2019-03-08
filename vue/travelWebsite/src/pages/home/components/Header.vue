b1087377c9e5868c154b4f38c293454f53960e43
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-08 14:18:12 +0800

    20190308

---
+++ vue/travelWebsite/src/pages/home/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header">
+    <div class="header-left"></div>
+    <div class="header-input"></div>
+    <div class="header-right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<!-- scoped关键字表明样式只对该组件有效，不会影响其他 -->
+<style lang="stylus" scoped>
+  .header
+    height: 0.5rem
+</style>
