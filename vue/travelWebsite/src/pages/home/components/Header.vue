624c50530823d61bed5ae75e64227a13a8a9f45a
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-09 15:39:54 +0800

    20190309

--- vue/travelWebsite/src/pages/home/components/Header.vue
+++ vue/travelWebsite/src/pages/home/components/Header.vue
@@ -23,33 +23,33 @@ export default {
 <!-- stylus引用失败，原因https://blog.csdn.net/weixin_37219302/article/details/79740464 -->
 <!-- scoped关键字表明样式只对当前组件有效，不会影响其他 -->
 <style lang="stylus" scoped>
-  /* 样式里引用样式必须加@符号 */
-  /* 通过在webpack.base.conf.js中对resolve下进行设置，可以使用~styles别名访问路径 */
-  @import '~styles/variables.styl';
-  .header
-    display: flex
-    line-height: .86rem
-    background: $bgColor
-    color: #fff
-    .header-left
-      width: .64rem
-      float: left
-      .back-icon
-        text-align: center
-        font-size: .4rem
-    .header-input
-      flex: 1
-      height: .64rem
-      line-height: .64rem
-      margin: .12rem 0 .12rem .2rem
-      padding-left: .2rem
-      background: #fff
-      border-redius: .1rem
-      color: #ccc
-    .header-right
-      width: 1.24rem
-      float: right
+/* 样式里引用样式必须加@符号 */
+/* 通过在webpack.base.conf.js中对resolve下进行设置，可以使用~styles别名访问路径 */
+@import '~styles/variables.styl';
+.header
+  display: flex
+  line-height: .86rem
+  background: $bgColor
+  color: #fff
+  .header-left
+    width: .64rem
+    float: left
+    .back-icon
       text-align: center
-      .arrow-icon
-        font-size: .24rem
+      font-size: .4rem
+  .header-input
+    flex: 1
+    height: .64rem
+    line-height: .64rem
+    margin: .12rem 0 .12rem .2rem
+    padding-left: .2rem
+    background: #fff
+    border-redius: .1rem
+    color: #ccc
+  .header-right
+    width: 1.24rem
+    float: right
+    text-align: center
+    .arrow-icon
+      font-size: .24rem
 </style>
