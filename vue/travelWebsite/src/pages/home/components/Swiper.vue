a9dbcdc80d7b47c070eeb1c82d1e3325ea4b0da1
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-09 12:42:49 +0800

    20190309

---
+++ vue/travelWebsite/src/pages/home/components/Swiper.vue
@@ -0,0 +1,31 @@
+<template>
+  <swiper :options="swiperOption">
+    <swiper-slide>
+      <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/e1abd1a9cfc4330d4d8794285350b4dd.jpg_750x200_a18c9dff.jpg" />
+    </swiper-slide>
+    <swiper-slide>
+      <img src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg" />
+    </swiper-slide>
+    <swiper-slide>I'm Slide 3</swiper-slide>
+    <swiper-slide>I'm Slide 4</swiper-slide>
+    <swiper-slide>I'm Slide 5</swiper-slide>
+    <swiper-slide>I'm Slide 6</swiper-slide>
+    <swiper-slide>I'm Slide 7</swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: []
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
