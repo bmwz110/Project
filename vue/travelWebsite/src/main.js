2a3e1a1e979d28732a3653c4c464c1675c287dca
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-03-09 11:35:07 +0800

    20190309

--- vue/travelWebsite/src/main.js
+++ vue/travelWebsite/src/main.js
@@ -5,8 +5,9 @@ import App from './App'
 import router from './router'
 // fastclick解决300ms点击延迟问题
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
--- vue/travelWebsite/src/pages/home/components/Header.vue
+++ vue/travelWebsite/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left"></div>
-    <div class="header-input"></div>
-    <div class="header-right"></div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
@@ -12,8 +20,36 @@ export default {
 }
 </script>
 
+<!-- stylus引用失败，原因https://blog.csdn.net/weixin_37219302/article/details/79740464 -->
 <!-- scoped关键字表明样式只对该组件有效，不会影响其他 -->
 <style lang="stylus" scoped>
+  /* 样式里引用样式必须加@符号 */
+  /* 通过在webpack.base.conf.js中对resolve下进行设置，可以使用~styles别名访问路径 */
+  @import '~styles/variables.styl';
   .header
-    height: 0.5rem
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      margin: .12rem 0 .12rem .2rem
+      padding-left: .2rem
+      background: #fff
+      border-redius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
 </style>
