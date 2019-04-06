9481edb82cddd6949ac8c250e8373a6e2a1dec83
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-04-06 19:41:41 +0800

    clear some error

---
+++ vue/deliverywebsite/deliverywebsite1.0/src/components/goods/goods.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>I am good</div>
+</template>
+
+<script>
+export default {
+  
+}
+
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
---
+++ vue/deliverywebsite/deliverywebsite1.0/src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    我是header
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
+
