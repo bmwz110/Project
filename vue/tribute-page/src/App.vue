3c98b459da340b4de205164a88d08066880a1e0b
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-04-16 12:35:59 +0800

    a tribute page for Dr. Norman Borlaug

---
+++ vue/tribute-page/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div id="app">
+      <Photo class="photo"/>
+      <Article class="article"/>
+    </div>
+    <div id="footer">
+      <hr>
+      <p>Written and coded by <a href="https://github.com/bmwz110">bmwz110</a>.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Photo from './components/photo/Photo'
+import Article from './components/article/Article'
+
+export default {
+  name: 'app',
+  components: {
+    Photo,
+    Article
+  }
+}
+
+function fontResize () {
+  var hW = document.body.offsetWidth
+  if (hW < 880) {
+    document.getElementsByClassName('article')[0].style.width = '90%'
+  } else {
+    document.getElementsByClassName('article')[0].style.width = '60%'
+  }
+}
+
+window.onresize = function () {
+  fontResize()
+}
+</script>
+<style lang="stylus">
+  #app
+    max-width: 1000px
+    width: 80%
+    margin: auto
+    margin-top: 50px
+    border-radius: 10px
+    background-color: rgb(238, 238, 238)
+    .photo
+      width: 100%
+    .article
+      width: 60%
+      margin: auto
+  #footer
+    width: 80%
+    margin: auto
+    text-align: center
+</style>
---
+++ vue/tribute-page/src/components/photo/Photo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="header text-center">
+      <h1 class="res">Dr. Norman Borlaug</h1>
+      <h3>The man who saved a billion lives</h3>
+    </div>
+    <div class="img-container">
+      <img class="img-responsive" src="./img.jpg">
+      <p>Dr. Norman Borlaug, third from left, trains biologists in Mexico on how to increase wheat yields - part of his life-long war on hunger.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Photo'
+}
+
+function fontResize () {
+  var hW = document.body.offsetWidth
+  if (hW < 880) {
+    document.getElementsByClassName('res')[0].style.fontSize = '3rem'
+  } else {
+    document.getElementsByClassName('res')[0].style.fontSize = '7rem'
+  }
+}
+
+window.onresize = function () {
+  fontResize()
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    margin: auto
+    .header
+      margin-top: 50px
+      h1
+        font-size: 7rem
+      h3
+        font-style:italic
+        font-size: 3rem
+    .img-container
+      background-color: white
+      margin: 10px 20px
+      padding: 0 5px 0 5px
+      text-align: center
+      p
+        margin-top: 6px
+</style>
