f1a7f0e7c7d807c792432ab8b04a8c322aac6744
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-04-29 09:22:30 +0800

    project init

--- vue/travel-app/src/App.vue
+++ vue/travel-app/src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- router-view显示当前路由地址所对应的内容 -->
     <router-view/>
   </div>
 </template>
--- vue/travel-app/src/main.js
+++ vue/travel-app/src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// fastclick需要npm install fastclick --save安装，解决部分设备点击延迟300ms问题
+import fastClick from 'fastclick'
+// reset.css解决不同设备默认样式不统一的问题
+import './assets/styles/reset.css'
+// border.css解决多倍屏1像素边框问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+// router   实例化路由，表示会使用
+// ---
+// 手动挂载 $mount 和 el 在效果上没有区别，都是为了把实例化的vue挂在到dom上
+// $mount可以实现延迟挂载
+// 在vue实例化中使用el，则实例化vue时会将此vue直接渲染到dom上
+// 没有在vue实例化中使用el，则该vue处于一个未挂载状态，可以使用$mount进行挂载
