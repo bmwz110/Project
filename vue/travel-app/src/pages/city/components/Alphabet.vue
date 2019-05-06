6879771baac999d846c05b169265026cac79d473
bmwz110 <bmwz110@mail.dlut.edu.cn>
2019-05-06 16:57:22 +0800

    list scroll

--- vue/travel-app/src/pages/city/components/Alphabet.vue
+++ vue/travel-app/src/pages/city/components/Alphabet.vue
@@ -2,10 +2,15 @@
   <ul class="list">
     <li
       class="item"
-      v-for="(item, key) of cities"
-      :key="key"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleLetterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
     >
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
@@ -15,6 +20,44 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        // 通过offsetTop拿到元素距离父元素顶部的值
+        const startY = this.$refs['A'][0].offsetTop
+        // 事件e的touches的第0项即表示手指信息
+        // 计算当前位置距离页面顶部的高 - 79
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 22)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 
