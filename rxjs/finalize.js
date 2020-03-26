d141c83075fdcd6ad9f0be15034860ab540a1fe1
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-03-26 14:06:12 +0800

    Create finalize.js

---
+++ rxjs/finalize.js
@@ -0,0 +1,17 @@
+const { of } = require('rxjs');
+const { map, finalize } = require('rxjs/operators');
+
+const nums = of(1, 2, 3);
+
+nums.pipe(
+  map(x => x * x),
+  finalize(() => console.log('finalize will run after subscribe finish'))
+).subscribe(
+  x => console.log(x)
+)
+
+
+// 1
+// 4
+// 9
+// finalize will run after subscribe finish
