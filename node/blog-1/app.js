7dade058642fc8029f09f41016d824c9d9dddbdc
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-04-18 09:17:01 +0800

    Update app.js

--- node/blog-1/app.js
+++ node/blog-1/app.js
@@ -1,17 +1,41 @@
-// nodemon 热更新
-// cross-env 设置环境
+const querystring = require('querystring')
+const handleBlogRouter = require('./src/router/blog')
+const handleUserRouter = require('./src/router/user')
+
+
 const serverHandle = (req, res) => {
+  // 设置返回格式为 JSON
   res.setHeader('Content-type', 'application/json')
 
-  const resData = {
-    name: 'bmwz110',
-    site: 'bmez110.com',
-    env: process.env.NODE_ENV
+  // Get path
+  const url = req.url
+  req.path = url.split('?')[0]
+
+  // resolve query para
+  req.query = querystring.parse(url.split('?')[0])
+  
+  // Dealing blog router
+  const blogData = handleBlogRouter(req, res)
+  if (blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return
   }
 
-  res.end(
-    JSON.stringify(resData)
-  )
+  // Dealing user router
+  const userData = handleUserRouter(req, res)
+  if (userData) {
+    res.end(
+      JSON.stringify(userData)
+    )
+    return
+  }
+
+  // Missing, return 404
+  res.writeHead(404, {"Content-type": "text/plain"})
+  res.write("404 Not Found\n")
+  res.end()
 }
 
 module.exports = serverHandle
