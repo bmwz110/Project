7507785d538571bd229ab63d07e3e9e7e4a65618
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-04-19 15:17:39 +0800

    Create index.js

---
+++ node/promise/index.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const path = require('path')
+
+// callback method to get file content
+function getFileContent(fileName, callback) {
+  const fullFileName = path.resolve(__dirname, 'files', fileName)
+  fs.readFile(fullFileName, (err, data) => {
+    if (err) {
+      console.log(err)
+      return
+    }
+    callback(
+      JSON.parse(data.toString())
+    )
+  })
+}
+
+// callback hell
+getFileContent('a.json', aData => {
+  console.log('aData: ', aData)
+  getFileContent(aData.next, bData => {
+    console.log('bData: ', bData);
+    getFileContent(bData.next, cData => {
+      console.log('cData: ', cData);
+    })
+  })
+})
+
+// Promise method to get file content
+function getFileContent(fileName) {
+  const promise = new Promise((resolve, reject) => {
+    const fullFileName = path.resolve(__dirname, 'files', fileName)
+
+    fs.readFile(fullFileName, (err, data) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(
+        JSON.parse(data.toString())
+      )
+    })
+  })
+  return promise
+}
+
+getFileContent('a.json').then(aData => {
+  console.log('aData: ', aData)
+  return getFileContent(aData.next)
+}).then(bData => {
+  console.log('bData: ', bData)
+  return getFileContent(bData.next)
+}).then(cData => {
+  console.log('cData: ', cData)
+  return getFileContent(cData.next)
+})
