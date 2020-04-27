fba1d7c80b62b43ca2046718110ed90b1824b5c3
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-04-27 19:48:05 +0800

    Create index.js

---
+++ node/mysql/index.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql')
+
+// create connection object
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Dvm1234!',
+  port: '3306',
+  database: 'myblog'
+})
+
+// start connection
+con.connect()
+
+// run sql command
+const sql = 'select * from users;'
+con.query(sql, (err, result) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(result);
+})
+
+// end connection
+con.end()
