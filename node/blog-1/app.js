a20b3f82df7263b8ae474ceb4840c7bf24a8fbf1
bmwz110 <bmwz110@mail.dlut.edu.cn>
2020-05-25 21:49:19 +0800

    Update app.js

--- node/blog-1/app.js
+++ node/blog-1/app.js
@@ -2,40 +2,124 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// session 数据
+const SESSION_DATA = {}
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  return d.toGMTString()
+}
 
 const serverHandle = (req, res) => {
   // 设置返回格式为 JSON
   res.setHeader('Content-type', 'application/json')
 
-  // Get path
+  // 获取 path 和 query
   const url = req.url
   req.path = url.split('?')[0]
+  req.query = querystring.parse(url.split('?')[1])
 
-  // resolve query para
-  req.query = querystring.parse(url.split('?')[0])
-  
-  // Dealing blog router
-  const blogData = handleBlogRouter(req, res)
-  if (blogData) {
-    res.end(
-      JSON.stringify(blogData)
-    )
-    return
+  // 解析 cookie
+  req.cookie = {}
+  const cookieStr = req.headers.cookie || ''
+  cookieStr.split(';').forEach(item => {
+    if (!item) {
+      return
+    }
+    const arr = item.split('=')
+    const key = arr[0].trim()
+    const val = arr[1].trim()
+    req.cookie[key] = val
+  })
+
+  // 解析 session
+  let needSetCookie = false
+  let userId = req.cookie.userid
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
   }
+  // 因为是 对象{} 的引用赋值，所以 req.session 的修改会同步到 SESSION_DATA
+  req.session = SESSION_DATA[userId]
 
-  // Dealing user router
-  const userData = handleUserRouter(req, res)
-  if (userData) {
-    res.end(
-      JSON.stringify(userData)
-    )
-    return
-  }
+  // 处理 POST data
+  getPostData(req).then(postData => {
+    req.body = postData
 
-  // Missing, return 404
-  res.writeHead(404, {"Content-type": "text/plain"})
-  res.write("404 Not Found\n")
-  res.end()
+    // Dealing blog router
+    const blogResult = handleBlogRouter(req, res)
+    if (blogResult) {
+      blogResult.then(blogData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
+        if (blogData) {
+          res.end(
+            JSON.stringify(blogData)
+          )
+          return
+        }
+      })
+      return
+    }
+
+    // Dealing user router
+    const userResult = handleUserRouter(req, res)
+    if (userResult) {
+      userResult.then(userData => {
+        if (needSetCookie) {
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
+        res.end(
+          JSON.stringify(userData)
+        )
+      })
+      return
+    }
+
+    // Missing, return 404
+    res.writeHead(404, {"Content-type": "text/plain"})
+    res.write("404 Not Found\n")
+    res.end()
+  })
+}
+
+// Dealing request
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({})
+      return
+    }
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({})
+      return
+    }
+
+    let postData = ''
+    req.on('data', chunk => {
+      postData += chunk.toString()
+    })
+    req.on('end', () => {
+      if (!postData) {
+        resolve({})
+        return
+      }
+      resolve(
+        JSON.parse(postData)
+      )
+    })
+  })
+  return promise
 }
 
 module.exports = serverHandle
